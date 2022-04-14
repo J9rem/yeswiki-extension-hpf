6589e7a46ac7940c6dd9c69114fefe048f705bbc
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-04-14 14:52:04 +0200

    fix(HpfPaymentStatusAction): do not show messages if not current entry

--- actions/HPFPaymentStatusAction.php
+++ actions/HPFPaymentStatusAction.php
@@ -11,7 +11,7 @@
 
 namespace YesWiki\Hpf;
 
-use YesWiki\Bazar\Service\FormManager;
+use YesWiki\Bazar\Service\EntryManager;
 use YesWiki\Core\YesWikiAction;
 use YesWiki\Core\Service\UserManager;
 use YesWiki\Hpf\Controller\HelloAssoController;
@@ -19,7 +19,7 @@ use YesWiki\Hpf\Controller\HelloAssoController;
 class HPFPaymentStatusAction extends YesWikiAction
 {
     protected $debug;
-    protected $formManager;
+    protected $entryManager;
     protected $helloAssoController;
 
     public function formatArguments($arg)
@@ -29,6 +29,7 @@ class HPFPaymentStatusAction extends YesWikiAction
             'pay_button_title' => $arg['pay_button_title'] ?? _t('HPF_PAY'),
             'nothing_to_pay_message' => $arg['nothing_to_pay_message'] ?? "",
             'view' => (empty($arg['view']) || !in_array($arg['view'], ['buttonHelloAsso','buttonYW','iframe'], true)) ? "buttonHelloAsso" : $arg['view'],
+            'entry_id' => isset($arg['entry_id']) && is_string($arg['entry_id']) ? $arg['entry_id'] : "",
         ]);
     }
 
@@ -36,7 +37,7 @@ class HPFPaymentStatusAction extends YesWikiAction
     {
         $this->debug = ($this->wiki->GetConfigValue('debug') =='yes');
         // get Services
-        $this->formManager = $this->getService(FormManager::class);
+        $this->entryManager = $this->getService(EntryManager::class);
         $this->helloAssoController = $this->getService(HelloAssoController::class);
         $this->userManager = $this->getService(UserManager::class);
 
@@ -45,33 +46,27 @@ class HPFPaymentStatusAction extends YesWikiAction
             return "";
         }
 
+        $contribFormId = $this->helloAssoController->getCurrentContribFormId();
         $contribEntry = $this->helloAssoController->getCurrentContribEntry($user['email']);
         if (empty($contribEntry)) {
+            if ($this->wiki->UserIsAdmin() && !empty($this->arguments['entry_id'])) {
+                $entry = $this->entryManager->getOne($this->arguments['entry_id']);
+                if (!empty($entry) && !empty($entry['id_typeannonce']) && !empty($entry['bf_mail']) && $entry['id_typeannonce'] == $contribFormId) {
+                    $this->updatePaymentsForEntry($entry, $entry['bf_mail']);
+                }
+            }
             return $this->arguments['empty_message'];
+        } elseif (!empty($this->arguments['entry_id']) && $this->arguments['entry_id'] != $contribEntry['id_fiche']) {
+            if ($this->wiki->UserIsAdmin()) {
+                $entry = $this->entryManager->getOne($this->arguments['entry_id']);
+                if (!empty($entry) && !empty($entry['id_typeannonce']) && !empty($entry['bf_mail']) && $entry['id_typeannonce'] == $contribFormId) {
+                    $this->updatePaymentsForEntry($entry, $entry['bf_mail']);
+                }
+            }
+            return "";
         }
-
-        $contribFormId = $this->helloAssoController->getCurrentContribFormId();
-        $form = $this->formManager->getOne($contribFormId);
-
-        // update CalcField before check
-        $newEntry = $this->helloAssoController->updateCalcFields($contribEntry, HelloAssoController::CALC_FIELDNAMES);
-        $previousValue = $contribEntry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
-        $newValue = $newEntry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
-        if ($previousValue != $newValue) {
-            $contribEntry = $this->helloAssoController->updateEntry($newEntry);
-        }
-
-        $calcValue = $contribEntry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
-
-        if (!empty($calcValue) && intval($calcValue) != 0) {
-            // refresh payments from HelloASso
-            $this->helloAssoController->refreshPaymentsInfo($user['email']);
-
-            // reload entry
-            $contribEntry = $this->helloAssoController->getCurrentContribEntry($user['email']);
-
-            $calcValue = $contribEntry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
-        }
+        
+        $calcValue = $this->updatePaymentsForEntry($contribEntry, $user['email']);
 
         if (empty($calcValue) || intval($calcValue) == 0) {
             return empty($this->arguments['nothing_to_pay_message']) ? "" :
@@ -130,5 +125,35 @@ class HPFPaymentStatusAction extends YesWikiAction
                     )
                 ]);
         }
+    }
+
+    /**
+     * @param array $entry
+     * @param string $email
+     * @return string|floatval $calcValue
+     */
+    private function updatePaymentsForEntry(array $entry, string $email)
+    {
+        // update CalcField before check
+        $newEntry = $this->helloAssoController->updateCalcFields($entry, HelloAssoController::CALC_FIELDNAMES);
+        $previousValue = $entry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
+        $newValue = $entry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
+        if ($previousValue != $newValue) {
+            $entry = $this->helloAssoController->updateEntry($newEntry);
+        }
+
+        $calcValue = $entry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
+
+        if (!empty($calcValue) && intval($calcValue) != 0) {
+            // refresh payments from HelloASso
+            $this->helloAssoController->refreshPaymentsInfo($email);
+
+            // reload entry
+            $entry = $this->helloAssoController->getCurrentContribEntry($email);
+
+            $calcValue = $entry[HelloAssoController::CALC_FIELDNAMES["total"]] ?? 0;
+        }
+
+        return $calcValue;
     }
 }
