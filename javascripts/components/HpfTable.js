d08b56ab18f8386e413ecc926175d499cda762ff
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-08-13 14:46:02 +0200

    refactor(HpfTable): usgin core DynTable

--- javascripts/components/HpfTable.js
+++ javascripts/components/HpfTable.js
@@ -7,6 +7,9 @@
  * file that was distributed with this source code.
  */
 
+import DynTable from '../../../bazar/presentation/javascripts/components/DynTable.js'
+import TemplateRenderer from '../../../bazar/presentation/javascripts/components/TemplateRenderer.js'
+
 const isVueJS3 = (typeof window.Vue.createApp == "function");
 
 const defaultValues = {}
@@ -28,35 +31,37 @@ export default {
         prop: 'isLoading',
         event: 'update-loading'
     },
+    components: {DynTable},
     props: ['isLoading'],
     data: function() {
         return {
             columns: [],
-            dataTable: null,
             params: null,
             payments: {...defaultData},
-            templatesForRendering: {},
+            rows: {},
             uuid: null,
             year: (new Date()).getFullYear()
-        };
+        }
     },
     computed:{
         element(){
             return isVueJS3 ? this.$el.parentNode : this.$el
+        },
+        sumtranslate(){
+            return TemplateRenderer.render('HpfPayementsTable',this,'sumtranslate')
         }
     },
-    methods: {
-        addRows(dataTable,columns){
-            const formattedDataList = []
+    methods:{
+        addRows(){
             Object.entries(this.payments).forEach(([id,row])=>{
                 const formattedData = {}
-                columns.forEach((col)=>{
+                this.columns.forEach((col)=>{
                     if (!(typeof col.data === 'string')){
                         formattedData[col.data] = ''
                     } else {
                         switch (col.data) {
                             case 'name':
-                                formattedData[col.data] = this.render('name',{['{id}']:(row?.id ?? id ) ?? 'unknown'})
+                                formattedData[col.data] = TemplateRenderer.render('HpfPayementsTable',this,'name',{},[['{id}',(row?.id ?? id ) ?? 'unknown']])
                                 break
                             case 'year':
                                 formattedData[col.data] = this.getSum(row)
@@ -67,10 +72,8 @@ export default {
                         }
                     }
                 })
-                formattedData.id = row?.id ?? id
-                formattedDataList.push(formattedData)
+                this.$set(this.rows,id,formattedData)
             })
-            dataTable.rows.add(formattedDataList)
         },
         getColumns(){
             if (this.columns.length == 0){
@@ -80,7 +83,7 @@ export default {
                 data.columns.push({
                     ...{
                         data: 'name',
-                        title: this.render('firstcolumntitle'),
+                        title: TemplateRenderer.render('HpfPayementsTable',this,'firstcolumntitle'),
                         footer: ''
                     },
                     ...width
@@ -89,7 +92,7 @@ export default {
                     ...{
                         data: 'year',
                         class: 'sum-activated',
-                        title: this.render('yeartotal'),
+                        title: TemplateRenderer.render('HpfPayementsTable',this,'yeartotal'),
                         footer: ''
                     },
                     ...width
@@ -113,7 +116,7 @@ export default {
                         ...{
                             data: key,
                             class: 'sum-activated',
-                            title: this.render(associations?.[key] ?? key),
+                            title: TemplateRenderer.render('HpfPayementsTable',this,associations?.[key] ?? key),
                             footer: ''
                         },
                         // ...width
@@ -122,57 +125,6 @@ export default {
                 this.columns = data.columns
             }
             return this.columns
-        },
-        getDatatableOptions(){
-            const buttons = []
-            DATATABLE_OPTIONS.buttons.forEach((option) => {
-              buttons.push({
-                ...option,
-                ...{ footer: true },
-                ...{
-                  exportOptions: (
-                    option.extend != 'print'
-                      ? {
-                        orthogonal: 'sort', // use sort data for export
-                        columns(idx, data, node) {
-                          return !$(node).hasClass('not-export-this-col')
-                        }
-                      }
-                      : {
-                        columns(idx, data, node) {
-                          const isVisible = $(node).data('visible')
-                          return !$(node).hasClass('not-export-this-col') && (
-                            isVisible == undefined || isVisible != false
-                          ) && !$(node).hasClass('not-printable')
-                        }
-                      })
-                }
-              })
-            })
-            return {
-                ...DATATABLE_OPTIONS,
-                ...{
-                  searching: true,// allow search but ue dom option not display filter
-                  dom:'lrtip', // instead of default lfrtip , with f for filter, see help : https://datatables.net/reference/option/dom
-                  footerCallback: ()=>{
-                    this.updateFooter()
-                  },
-                  buttons
-                }
-              }
-        },
-        getDatatable(){
-            if (this.dataTable === null){
-                // create dataTable
-                const columns = this.getColumns()
-                const options = this.getDatatableOptions()
-                options.columns = columns
-                options['scrollX'] = true
-                this.dataTable = $(this.$refs.dataTable).DataTable(options)
-                $(this.dataTable.table().node()).prop('id',this.getUuid())
-                this.initFooter(columns)
-            }
-            return this.dataTable
         },
         getSum(row){
             return Object.entries(row.values)
@@ -186,54 +138,11 @@ export default {
                 })
                 .reduce((sum,[,value])=>sum += value,0)
         },
-        getTemplateFromSlot(name,params){
-            const key = name+'-'+JSON.stringify(params)
-            if (!(key in this.templatesForRendering)){
-                if (name in this.$scopedSlots){
-                    const slot = this.$scopedSlots[name]
-                    const constructor = window.Vue.extend({
-                        render: function(h){
-                            return h('div',{},slot(params))
-                        }
-                    })
-                    const instance = new constructor()
-                    instance.$mount()
-                    let outerHtml = '';
-                    for (let index = 0; index < instance.$el.childNodes.length; index++) {
-                        outerHtml += instance.$el.childNodes[index].outerHTML || instance.$el.childNodes[index].textContent
-                    }
-                    this.templatesForRendering[key] = outerHtml
-                } else {
-                    this.templatesForRendering[key] = name
-                }
-            }
-            return this.templatesForRendering[key]
-        },
         getUuid(){
             if (this.uuid === null){
                 this.uuid = crypto.randomUUID()
             }
             return this.uuid
-        },
-        initFooter(columns){
-            const footerNode = this.dataTable.footer().to$()
-            if (footerNode[0] !== null){
-                const footer = $('<tr>')
-                let displayTotal = true
-                columns.forEach((col)=>{
-                    if ('footer' in col && col.footer.length > 0){
-                        const element = $(col.footer)
-                        const isTh = $(element).prop('tagName') === 'TH'
-                        footer.append(isTh ? element : $('<th>').append(element))
-                    } else if (displayTotal) {
-                        displayTotal = false
-                        footer.append($('<th>').text(this.render('sum')))
-                    } else {
-                        footer.append($('<th>'))
-                    }
-                })
-                footerNode.html(footer)
-            }
         },
         manageError(error = null){
             if (error && wiki.isDebugEnabled){
@@ -241,22 +150,11 @@ export default {
             }
             return null
         },
-        removeRows(dataTable){
-            dataTable.rows((idx,data,node)=>{
-                return data?.id > 0 && data?.id < 5
-            }).remove()
+        removeRows(){
+            Object.keys(this.rows).forEach((id)=>{
+                this.$delete(this.rows,id)
+            })
         },
-        render(name,replacement = {}){
-            let output = this.getTemplateFromSlot(name,{})
-            Object.entries(replacement).forEach(([anchor,replacement]) => {
-                output = output.replace(anchor,replacement)
-            });
-            return output
-        },
-        sanitizeValue(val) {
-            const sanitizedValue = (typeof val === 'object') ? (val?.display || '') : val
-            return (isNaN(sanitizedValue)) ? 1 : Number(sanitizedValue)
-          },
         async secureResetIsloading(){
             let shouldReset = true
             const unwatch = this.$watch('isLoading',()=>{
@@ -270,34 +168,15 @@ export default {
             },5000)
         },
         updatePayments(){
-            const dataTable = this.getDatatable()
-            const columns = this.getColumns()
-            this.removeRows(dataTable)
-            this.addRows(dataTable,columns)
-            dataTable.draw()
-        },
-        updateFooter(){
-            if (this.dataTable !== null){
-                const activatedRows = []
-                this.dataTable.rows({ search: 'applied' }).every(function() {
-                  activatedRows.push(this.index())
-                })
-                this.dataTable.columns('.sum-activated').every((indexCol) => {
-                    let col = this.dataTable.column(indexCol)
-                    let sum = 0
-                    activatedRows.forEach((indexRow) => {
-                      const value = this.dataTable.row(indexRow).data()[col.dataSrc()]
-                      sum += this.sanitizeValue(Number(value))
-                    })
-                    this.dataTable.footer().to$().find(`> tr > th:nth-child(${indexCol+1})`).html(sum)
-                })
-            }
+            this.getColumns()
+            this.removeRows()
+            this.addRows()
         },
     },
     mounted(){
         $(this.element).on('dblclick',function(e) {
             return false;
-          });
+        });
         const rawParams = this.element.dataset?.params
         if (rawParams){
             try {
@@ -306,7 +185,7 @@ export default {
                 console.error(error)
             }
         }
-        this.secureResetIsloading()
+        this.secureResetIsloading().catch(this.manageError)
         this.updatePayments() // to force display table
         // example waiting loading
         this.payments[3].values[6] = 60
@@ -323,10 +202,10 @@ export default {
     },
     template: `
     <div>
-        <table ref="dataTable" class="table prevent-auto-init table-condensed display">
-            <tfoot>
-                <tr></tr>
-            </tfoot>
-        </table>
-    </div>`
+        <dyn-table :columns="columns" :rows="rows">
+            <template #dom>&lt;'row'&lt;'col-sm-12'tr>>&lt;'row'&lt;'col-sm-6'i>&lt;'col-sm-6'&lt;'pull-right'B>>></template>
+            <template #sumtranslate>{{ sumtranslate }}</template>
+        </dyn-table>
+    </div>
+    `
 }
